014b2fcba2f2960e62d6abf32fa31fa90bed3752
Stefan Andres <s.andres@syseleven.de>
2014-12-19 09:44:40 +0100

    os-160 add option to have multiple default handler and implement mailer handler

--- manifests/profile/server/handlers.pp
+++ manifests/profile/server/handlers.pp
@@ -1,37 +1,18 @@
 class sys11sensu::profile::server::handlers(
-  $hipchat_settings = false,
+  $default_handlers = []
 ) {
-  require sys11sensu::profile::server::handlers_dep
 
-  if $hipchat_settings {
-    file {'/etc/sensu/conf.d/handlers/hipchat_settings.json':
-      ensure  => file,
-      mode    => '0444',
-      content => template("$module_name/hipchat_settings.json.erb"),
-    }
+  define load_handler() {
+    class { "$name": }
+  }
 
-    file {'/etc/sensu/handlers/hipchat.rb':
-      ensure => file,
-      source => "puppet:///modules/$module_name/handlers/hipchat.rb",
-    }
-
-    exec {'hipchat-gem':
-      command     => 'gem1.9.3 install hipchat --no-rdoc --no-ri',
-      unless      => 'gem1.9.3 list hipchat | grep hipchat',
-      path        => '/bin:/usr/bin',
-    }
-
+  if $default_handlers {
     sensu::handler {'default':
-      command => '/etc/sensu/handlers/hipchat.rb',
-      type    => 'pipe',
+      command  => true,
+      type     => 'set',
+      handlers => $default_handlers
     }
-  }
 
-  exec {'sensu-plugin-gem':
-    command     => 'gem1.9.3 install sensu-plugin --no-rdoc --no-ri',
-    unless      => 'gem1.9.3 list sensu-plugin | grep sensu-plugin',
-    path        => '/bin:/usr/bin',
+    load_handler { $default_handlers: }
   }
-
 }
-
---
+++ manifests/profile/server/handlers/mailer.pp
@@ -0,0 +1,28 @@
+class sys11sensu::profile::server::handlers::mailer(
+  $settings,
+) {
+  require sys11sensu::profile::server::handlers_dep
+
+  file {'/etc/sensu/conf.d/handlers/mailer_settings.json':
+    ensure  => file,
+    mode    => '0444',
+    content => template("$module_name/handlers/mailer_settings.json.erb"),
+  }
+
+  file {'/etc/sensu/handlers/mailer.rb':
+    ensure => file,
+    source => "puppet:///modules/$module_name/handlers/mailer.rb",
+  } 
+
+  exec {'mailer-gem':
+    command     => 'gem1.9.3 install mailer --no-rdoc --no-ri',
+    unless      => 'gem1.9.3 list mailer | grep mailer',
+    path        => '/bin:/usr/bin',
+  }
+
+  sensu::handler {'mailer_handler':
+      command => '/etc/sensu/handlers/mailer.rb',
+      type    => 'pipe',
+  }
+}
+
