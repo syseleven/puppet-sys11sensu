727c58883c2c5e0af241ca76bcbb62b5d139d544
Stefan Andres <s.andres@syseleven.de>
2014-12-19 11:22:29 +0100

    os-163 fix dependency

--- manifests/profile/server/handlers_dep.pp
+++ manifests/profile/server/handlers_dep.pp
@@ -1,5 +1,5 @@
 class sys11sensu::profile::server::handlers_dep() {
-  ensure_packages(['ruby1.9.3', 'make'])
+  ensure_packages(['ruby1.9.3', 'ruby1.9.1-dev', 'make'])
   exec {'sensu-plugin-gem':
     command     => 'gem1.9.3 install sensu-plugin --no-rdoc --no-ri',
     unless      => 'gem1.9.3 list sensu-plugin | grep sensu-plugin',
