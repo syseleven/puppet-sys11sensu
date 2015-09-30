adc0300f6942a6e3ec217fc1a39849a8b4a2a9a8
Stefan Andres <s.andres@syseleven.de>
2015-09-30 10:59:26 +0200

    os-943 workaround sensu-client restarting issue

--- manifests/profile/common.pp
+++ manifests/profile/common.pp
@@ -7,4 +7,10 @@ class sys11sensu::profile::common(
     before => Package['sensu-plugin'],
   }
 
+  file { '/etc/init.d/sensu-service':
+    ensure => file,
+    mode   => '0555',
+    before => Service['sensu-client'],
+    source => "puppet:///modules/$module_name/init_d_sensu-service",
+  }
 }
