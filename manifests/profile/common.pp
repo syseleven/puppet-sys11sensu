ae11dbddb46fe8555201e179757c514fff5458a4
Stefan Andres <s.andres@syseleven.de>
2015-09-30 11:09:27 +0200

    os-943 require package for init script

--- manifests/profile/common.pp
+++ manifests/profile/common.pp
@@ -8,9 +8,10 @@ class sys11sensu::profile::common(
   }
 
   file { '/etc/init.d/sensu-service':
-    ensure => file,
-    mode   => '0555',
-    before => Service['sensu-client'],
-    source => "puppet:///modules/$module_name/init_d_sensu-service",
+    ensure  => file,
+    mode    => '0555',
+    source  => "puppet:///modules/$module_name/init_d_sensu-service",
+    before  => Service['sensu-client'],
+    require => Package['sensu'],
   }
 }
