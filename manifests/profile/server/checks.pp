a09a7472ef1c98d8947dda7526753312644deebf
Johannes Grassler <j.grassler@syseleven.de>
2016-01-12 11:22:03 +0100

    os-592 Implemented mechanism for custom checks
    This commit adds the `extra` parameter to
    sys11sensu::profile::server::checks::extra. This parameter takes a hash
    keyed by check names with the values being parameters to the
    sensu::check resource. You can use this parameter to specify additional
    checks to be run on a Sensu server (e.g. a check for monitoring a remote
    web server's reachability.

--- manifests/profile/server/checks.pp
+++ manifests/profile/server/checks.pp
@@ -1,7 +1,17 @@
 class sys11sensu::profile::server::checks(
   $uchiwa_port = hiera('sys11sensu::uchiwa_port', 3001),
+  $extra = []   # Additional checks to run from this server. Takes a hash of check names
+                # with parameter hashes for values.
 ) {
   ensure_packages(['nagios-plugins-basic'])
+
+  if ! empty($extra) {
+    $checknames = keys($extra)
+
+    extras{$checknames:
+      params => $extra
+    }
+  }
 
   sensu::check { 'sensu-dashboard-http':
     command => "PATH=\$PATH:/usr/lib/nagios/plugins check_http -H localhost -p $uchiwa_port",
@@ -14,9 +24,14 @@ class sys11sensu::profile::server::checks(
   sensu::check { 'sensu-rabbitmq':
     command => '/usr/lib/nagios/plugins/check_tcp -H localhost -p 5672',
   }
-  
+
   sensu::check { 'sensu-redis':
     command => '/usr/lib/nagios/plugins/check_tcp -H localhost -p 6379',
   }
-  
+
+  # Wrapper for processing $checks hash.
+  define extras($params){
+    notice("creating Sensu::Check[$name]")
+    ensure_resource('sensu::check', $name, $params[$name])
+  }
 }
